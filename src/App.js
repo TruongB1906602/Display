8df529a323f006010c64dc019ec53d9fce43e4c9
TruongB1906602 <truongb1906602@student.ctu.edu.vn>
2023-02-24 16:25:20 +0700

    display

--- src/App.js
+++ src/App.js
@@ -2,14 +2,30 @@
 import './App.css';
 import queryString from 'query-string';
 function App() {
-  const {name, price, description } = queryString.parse(window.location.href);
+  const {id,name, price, description,exprire_date,img } = queryString.parse(window.location.href);
   return (
     <div className="App">
+      <div>
+         <a> <img src={img}/></a>
+      </div> 
+      <h3>Thông sản phẩm</h3>
+       <div  className='infor'>
+        <p>Mã SP: {id}</p>
+        <p>Hạn sản xuất {exprire_date}</p>
         <p>Tên sản phẩm: {name}</p>
         <p>Giá: {price}</p>
-        <p>Mô tả:{description}</p>
+
+       </div>
+      
+       <div >
+           <h3>Mô tả</h3>
+           <p>{description}</p>
+
+
+       </div>
     </div>
   );
+  
 }
 
 export default App;
